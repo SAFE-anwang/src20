f6ca9675dced9945d4c68477aa1e0fe6dc4cd8a2
lemengbin <lemengbin@163.com>
2025-09-08 11:08:32 +0800

    update SRC20Lock

--- contracts/SRC20/SRC20Lock.sol
+++ contracts/SRC20/SRC20Lock.sol
@@ -3,14 +3,19 @@ pragma solidity ^0.8.0;
 
 contract SRC20Lock {
     uint256 no;
-
-    mapping(address => uint256) addr2amount;
-    mapping(address => mapping(uint256 => LockRecord)) addr2locks;
     mapping(address => uint256[]) addr2ids;
+    mapping(uint256 => LockRecord) id2record;
     mapping(uint256 => uint256) id2index;
-    mapping(uint256 => address) id2addr;
 
-    address internal constant PROPERTY_ADDR = 0x0000000000000000000000000000000000001000;
+    uint256 min_lock_amount;
+
+    bool internal flag; // re-entrant lock
+    modifier noReentrant() {
+        require(!flag, "Error: reentrant call");
+        flag = true;
+        _;
+        flag = false;
+    }
 
     struct LockRecord {
         uint256 id;
@@ -21,34 +26,54 @@ contract SRC20Lock {
         uint256 unlockHeight;
     }
 
-    bool internal lock; // re-entrant lock
-    modifier noReentrant() {
-        require(!lock, "Error: reentrant call");
-        lock = true;
-        _;
-        lock = false;
-    }
+    event Lock(address _token, address _addr, uint256 _amount, uint256 _lockDay, uint256 _id);
+    event Withdraw(address _token, address _addr, uint256 _amount, uint256[] _ids);
 
-    event SRC20Deposit(address _addr, uint256 _id, uint256 _amount, uint256 _lockDay, uint256 _unlockHeight);
-    event SRC20Withdraw(address _addr, uint256 _amount, uint256[] _ids);
-
-    function deposit(address _to, uint256 _lockDay) public payable returns (uint256) {
+    function lock(address _token, address _to, uint256 _amount, uint256 _lockDay) public returns (uint256) {
+        require(_token != address(0), "invalid token address");
+        require(_amount > 0, "invalid amount");
+        require(_amount >= min_lock_amount, "less than min_lock_amount");
         require(_lockDay > 0, "invalid lock day");
-        require(msg.value >= getMinLockAmount(), "invalid lock amount");
-        LockRecord memory record = addRecord(_to, msg.value, _lockDay);
-        emit SRC20Deposit(_to, record.id, msg.value, _lockDay, record.unlockHeight);
-        return record.id;
+
+        (bool success, bytes memory data) = _token.call(abi.encodeWithSignature("transferFrom(address,address,uint256)", msg.sender, address(this), _amount));
+        if(!success) revert(getRevertMessage(data));
+
+        uint id = addRecord(_to, _amount, _lockDay);
+        emit Lock(_token, _to, _amount, _lockDay, id);
+        return id;
     }
 
-    function withdrawByID(uint256[] memory _ids) public noReentrant returns (uint256) {
+    function batchLock(address _token, address _to, uint256 _amount, uint256 _times, uint256 _spaceDay, uint256 _startDay) public returns (uint256[] memory) {
+        require(_token != address(0), "invalid token address");
+        require(_amount > 0, "invalid amount");
+        require(_times > 0, "invalid times");
+        require(_amount / _times >= min_lock_amount, "_amount/_times less than min_lock_amount");
+        require(_spaceDay + _startDay > 0, "_spaceDay + _startDay can't be 0");
+
+        (bool success, bytes memory data) = _token.call(abi.encodeWithSignature("transferFrom(address,address,uint256)", msg.sender, address(this), _amount));
+        if(!success) revert(getRevertMessage(data));
+
+        uint256[] memory ids = new uint256[](_times);
+        uint batchAmount = _amount / _times;
+        uint i;
+        for(; i < _times - 1; i++) {
+            ids[i] = addRecord(_to, batchAmount, _startDay + (i + 1) * _spaceDay);
+            emit Lock(_token, _to, batchAmount, _startDay + (i + 1) * _spaceDay, ids[i]);
+        }
+        ids[i] = addRecord(_to, batchAmount + _amount % _times, _startDay + (i + 1) * _spaceDay);
+        emit Lock(_token, _to, batchAmount + _amount % _times, _startDay + (i + 1) * _spaceDay, ids[i]);
+        return ids;
+    }
+
+    function withdrawByID(address _token, uint256[] memory _ids) public noReentrant returns (uint256) {
         require(_ids.length > 0 && _ids.length <= 30, "invalid ids size, min: 1, max: 30");
         uint256 amount;
         LockRecord memory record;
         uint256[] memory temps = new uint256[](_ids.length);
         uint256 k;
         for(uint256 i; i < _ids.length; i++) {
-            record = addr2locks[msg.sender][_ids[i]];
-            if(record.amount == 0 || block.number < record.unlockHeight) {
+            record = id2record[_ids[i]];
+            if(record.amount == 0 || record.addr != tx.origin || block.number < record.unlockHeight) {
                 continue;
             }
             amount += record.amount;
@@ -56,12 +81,13 @@ contract SRC20Lock {
             temps[k++] = _ids[i];
         }
         if(amount != 0) {
-            payable(msg.sender).transfer(amount);
+            (bool success, bytes memory data) = _token.call(abi.encodeWithSignature("transfer(address,uint256)", tx.origin, amount));
+            if(!success) revert(getRevertMessage(data));
             uint256[] memory retIDs = new uint256[](k);
             for(uint256 i; i < k; i++) {
                 retIDs[i] = temps[i];
             }
-            emit SRC20Withdraw(msg.sender, amount, retIDs);
+            emit Withdraw(_token, tx.origin, amount, retIDs);
         }
         return amount;
     }
@@ -71,18 +97,19 @@ contract SRC20Lock {
     }
 
     function getTotalIDs(address _addr, uint256 _start, uint256 _count) public view returns (uint256[] memory) {
-        uint256 num = getTotalIDNum(_addr);
-        require(num > 0, "insufficient quantity");
-        require(_start < num, "invalid _start, must be in [0, totalNum)");
+        uint256 totalNum = getTotalIDNum(_addr);
+        require(totalNum > 0, "insufficient quantity");
+        require(_start < totalNum, "invalid _start, must be in [0, totalIDNum)");
         require(_count > 0 && _count <= 100, "max return 100 ids");
 
-        uint256[] memory ret = new uint256[](_count);
+        uint256 num = _count;
+        if(_start + _count >= totalNum) {
+            num = totalNum - _start;
+        }
+        uint256[] memory ret = new uint256[](num);
         uint256[] memory ids = addr2ids[_addr];
-        for(uint256 i; i < _count; i++) {
-            if(i + _start >= ids.length) {
-                break;
-            }
-            ret[i++] = ids[i + _start];
+        for(uint256 i; i < num; i++) {
+            ret[i] = ids[i + _start];
         }
         return ret;
     }
@@ -91,7 +118,7 @@ contract SRC20Lock {
         uint256 num;
         uint256[] memory ids = addr2ids[_addr];
         for(uint256 i; i < ids.length; i++) {
-            if(block.number >= addr2locks[_addr][ids[i]].unlockHeight) {
+            if(block.number >= id2record[ids[i]].unlockHeight) {
                 num++;
             }
         }
@@ -108,7 +135,7 @@ contract SRC20Lock {
         uint256 index;
         uint256[] memory ids = addr2ids[_addr];
         for(uint256 i; i < ids.length; i++) {
-            if(block.number >= addr2locks[_addr][ids[i]].unlockHeight) {
+            if(block.number >= id2record[ids[i]].unlockHeight) {
                 temp[index++] = ids[i];
             }
         }
@@ -128,7 +155,7 @@ contract SRC20Lock {
         uint256 num;
         uint256[] memory ids = addr2ids[_addr];
         for(uint256 i; i < ids.length; i++) {
-            if(block.number < addr2locks[_addr][ids[i]].unlockHeight) {
+            if(block.number < id2record[ids[i]].unlockHeight) {
                 num++;
             }
         }
@@ -145,7 +172,7 @@ contract SRC20Lock {
         uint256 index;
         uint256[] memory ids = addr2ids[_addr];
         for(uint256 i; i < ids.length; i++) {
-            if(block.number < addr2locks[_addr][ids[i]].unlockHeight) {
+            if(block.number < id2record[ids[i]].unlockHeight) {
                 temp[index++] = ids[i];
             }
         }
@@ -161,60 +188,47 @@ contract SRC20Lock {
         return ret;
     }
 
-    function getRecordByID(uint256 _id) public view returns (LockRecord memory ret) {
-        return addr2locks[id2addr[_id]][_id];
+    function getRecordByID(uint256 _id) public view returns (LockRecord memory) {
+        return id2record[_id];
     }
 
     // add record
-    function addRecord(address _addr, uint256 _amount, uint256 _lockDay) internal returns (LockRecord memory) {
-        uint256 startHeight = block.number;
-        uint256 unlockHeight = startHeight + _lockDay * getNumberInDay();
-        uint256 id = ++no;
-        LockRecord memory record = LockRecord(id, _addr, _amount, _lockDay, startHeight, unlockHeight);
-        addr2amount[_addr] += _amount;
-        addr2locks[_addr][id] = record;
+    function addRecord(address _addr, uint256 _amount, uint256 _lockDay) internal returns (uint256) {
+        require(_lockDay != 0, "invalid lock day");
+        uint unlockHeight = block.number + _lockDay * getNumberInDay();
+        uint id = ++no;
         addr2ids[_addr].push(id);
+        id2record[id] = LockRecord(id, _addr, _amount, _lockDay, block.number, unlockHeight);
         id2index[id] = addr2ids[_addr].length - 1;
-        id2addr[id] = _addr;
-        return record;
+        return id;
     }
 
     // delete record
     function delRecord(uint256 _id) internal {
-        uint256 pos = id2index[_id];
-        address addr = id2addr[_id];
-        uint256 amount = addr2locks[addr][_id].amount;
-
-        if(addr2amount[addr] < amount) {
-            addr2amount[addr] = 0;
-        } else {
-            addr2amount[addr] -= amount;
-        }
-
-        delete addr2locks[addr][_id];
-
-        uint256[] storage ids = addr2ids[addr];
-        if(ids.length > 0) {
-            ids[pos] = ids[ids.length - 1];
-            ids.pop();
-            if(ids.length > 0) {
-                id2index[ids[pos]] = pos;
-            }
-        }
-
+        require(id2record[_id].addr == tx.origin, "invalid record id");
+        uint[] storage ids = addr2ids[tx.origin];
+        uint pos = id2index[_id];
+        ids[pos] = ids[ids.length - 1];
+        id2index[ids[pos]] = pos;
+        ids.pop();
+        delete id2record[_id];
         delete id2index[_id];
-        delete id2addr[_id];
-    }
-
-    function getMinLockAmount() internal view returns (uint256) {
-        (bool success, bytes memory data) = PROPERTY_ADDR.staticcall(abi.encodeWithSignature("getValue(string)", "deposit_min_amount"));
-        require(success, "get deposit_min_amount failed");
-        return abi.decode(data, (uint256));
     }
 
     function getNumberInDay() internal view returns (uint256) {
-        (bool success, bytes memory data) = PROPERTY_ADDR.staticcall(abi.encodeWithSignature("getValue(string)", "block_space"));
-        require(success, "get block_space failed");
+        (bool success, bytes memory data) = 0x0000000000000000000000000000000000001000.staticcall(abi.encodeWithSignature("getValue(string)", "block_space"));
+        if(!success) revert(getRevertMessage(data));
         return 86400 / abi.decode(data, (uint256));
+    }
+
+    function getRevertMessage(bytes memory _data) internal pure returns (string memory) {
+        if (_data.length > 0) {
+            assembly {
+                let size := mload(_data)
+                revert(add(32, _data), size)
+            }
+        } else {
+            return "unknown error";
+        }
     }
 }
